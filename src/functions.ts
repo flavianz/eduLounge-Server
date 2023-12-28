d6414684c20b75728bd1d21765b415bd55e4b87f
Flavianthepavian <flavianzuellig@hotmail.com>
2023-12-28 13:25:56 +0100

    Add encrypt/decrypt Object function

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,5 @@
+import {decrypt, encrypt} from "./database/crypto.js";
+
 /** Generate a random string using the characters a-z, A-Z and 0-9
  * @param {number} length the length of the generated string, default 32
  * @returns {string} the generated string
@@ -9,4 +11,30 @@ function getRandomString(length: number = 32): string {
         result += chars[Math.floor(Math.random() * chars.length)];
     }
     return result;
+}
+
+export function encryptObject(obj:object, encryptValues=true): object {
+    function modifyValue(value: string): string
+    {
+        return encryptValues ? encrypt(value) : decrypt(value)
+    }
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            if (Array.isArray(obj[key])) {
+                // If the value is an array, modify each element
+                obj[key] = obj[key].map((element: any) => {
+                    return typeof element === 'object' && element !== null
+                        ? encryptObject(element, encryptValues) // Recursively modify nested objects
+                        : modifyValue(element); // Modify non-object values
+                });
+            } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+                // If the value is an object, recursively modify its values
+                obj[key] = encryptObject(obj[key], encryptValues);
+            } else {
+                // Modify the value (you can customize this part based on your requirements)
+                obj[key] = modifyValue(obj[key]);
+            }
+        }
+    }
+    return obj;
 }
