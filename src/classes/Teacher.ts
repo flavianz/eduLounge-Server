0877bab48736b5791c7a4fc074dab3eb31a2d197
Flavianthepavian <flavianzuellig@hotmail.com>
2023-12-28 22:35:04 +0100

    Add createTeacher function

--- src/classes/Teacher.ts
+++ src/classes/Teacher.ts
@@ -1,8 +1,21 @@
 import {User} from "./User.js";
+import {PersonalInfo} from "../types.js";
+import {generateUUID, hash} from "../database/crypto.js";
+import {dbQuery} from "../database/postgres.js";
+import {encryptObject} from "../functions.js";
 
 export class Teacher extends User {
     constructor(uuid: string) {
         super(uuid);
     }
-
+    static async createTeacher(password: string, school_id: string, personalInfo: PersonalInfo): Promise<User>
+    {
+        try {
+            const uuid = generateUUID();
+            await dbQuery("INSERT INTO accounts.teachers (user_id, password, school_id, personal_info) VALUES ($1, $2, $3, json_populate_record(null::accounts.personal_info, $4))", [uuid, hash(password), school_id, JSON.stringify(encryptObject(personalInfo))]);
+            return new Teacher(uuid)
+        }catch (e) {
+            throw new Error("Failed to create user in database", {cause: e})
+        }
+    }
 }
