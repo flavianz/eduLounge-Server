655cfe0207517ba1b52e8d090f82d3434830d9b6
Flavianthepavian <survivallist.net@gmail.com>
2024-01-03 11:13:37 +0100

    Get db url from .env

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,26 +1,24 @@
 import pg from "pg";
+import { configDotenv } from "dotenv";
+configDotenv();
+
 const client = new pg.Client({
-    host: "localhost",
-    port: 5432,
-    database: "edulounge",
-    user: "postgres",
-    password: "flazu66.100%",
+    connectionString: process.env.DATABASEURL,
 });
 await client.connect();
+
 /**Execute a query in the database
  * @param {string} query the sql query to be executed
  * @param {string[]} values the values to be inserted in placed annotated using '$'
  * @returns the query result of the executed query
  * */
-export async function dbQuery(query: string, values?: string[]): Promise<pg.QueryResult> {
-    return await new Promise(async (resolve) => {
-        try
-        {
-            resolve(await client.query(query, values))
-        }
-        catch (e)
-        {
-            throw new Error("Statement failed", {cause: e})
-        }
-    });
+export async function dbQuery(
+    query: string,
+    values?: string[],
+): Promise<pg.QueryResult> {
+    try {
+        return await client.query(query, values);
+    } catch (e) {
+        throw new Error("Statement failed", { cause: e });
+    }
 }
