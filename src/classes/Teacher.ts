aafaa2d8f05f5194ecd73dcda3e3f995321fff63
Flavianthepavian <flavianzuellig@hotmail.com>
2024-01-09 15:29:27 +0100

    remove personal info id and add username param to create

--- src/classes/Teacher.ts
+++ src/classes/Teacher.ts
@@ -1,22 +1,28 @@
 import { User } from "./User.js";
-import { generateUUID, hash } from "../database/crypto.js";
+import { encrypt, generateUUID, hash } from "../database/crypto.js";
 import { insert } from "../database/queries.js";
 
 export class Teacher extends User {
     constructor(uuid: string) {
         super(uuid);
     }
+    /** Create a teacher in the database
+     * @param {string} password The password for the teacher
+     * @param {string} school_id The teacher's school's id
+     * @param {string} username The username for the teacher
+     * @returns {Teacher}, a class instance of the created teacher in the database
+     * */
     static async createTeacher(
         password: string,
         school_id: string,
-        personalInfoID: string,
+        username: string,
     ): Promise<Teacher> {
         const uuid = generateUUID();
         await insert(
             "teachers",
-            ["user_id", "password", "school_id", "personal_info_id"],
+            ["user_id", "password", "school_id", "username"],
             uuid,
-            [hash(password), school_id, personalInfoID],
+            [hash(password), school_id, encrypt(username)],
             false,
         );
         return new Teacher(uuid);
