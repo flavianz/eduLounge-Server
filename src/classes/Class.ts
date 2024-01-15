523df33ba7c4ac6598ca5639d190855bb2b35e75
flavi <flavianzuellig@hotmail.com>
2024-01-15 07:29:12 +0100

    fix and document createUser

--- src/classes/Class.ts
+++ src/classes/Class.ts
@@ -1,17 +1,29 @@
-import { generateUUID } from "../database/crypto.js";
+import { encrypt, generateUUID } from "../database/crypto.js";
 import { DatabaseColumn } from "./DatabaseColumn.js";
 import { insert } from "../database/queries.js";
-
 export class Class extends DatabaseColumn {
     constructor(uuid: string) {
         super(uuid, "classes", "class_id");
     }
     /**Create a new class in the database
+     * @param {string} grade the grade the class is in
+     * @param {(string | Student)[]} students the students attending the classes
+     * @param {string} teacher the teacher of the class
      * @returns {Class}, the created class instance
      * */
-    static async createClass(grade: string): Promise<Class> {
+    static async createClass(
+        grade: string,
+        students: string[],
+        teacher: string,
+    ): Promise<Class> {
         const uuid = generateUUID();
-        await insert("classes", ["class_id", "grade"], uuid, [grade]);
+        await insert(
+            "classes",
+            ["class_id", "grade", "teacher_id"],
+            uuid,
+            [encrypt("grade"), teacher],
+            false,
+        );
         return new Class(uuid);
     }
     /** Get the grade of the class
