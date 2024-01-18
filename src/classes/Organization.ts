4ed2c0a443e5090737c4ef1b141b2d7de2d9e272
flavi <flavianzuellig@hotmail.com>
2024-01-18 23:23:13 +0100

    rename Organisation to Organisation

--- src/classes/Organization.ts
+++ src/classes/Organization.ts
@@ -2,45 +2,45 @@ import { generateUUID } from "../database/crypto.js";
 import { insert } from "../database/queries.js";
 import { DatabaseColumn } from "./DatabaseColumn.js";
 
-export class Organisation extends DatabaseColumn {
+export class Organization extends DatabaseColumn {
     constructor(uuid: string) {
-        super(uuid, "organisations", "organisation_id");
+        super(uuid, "organizations", "organization_id");
     }
-    /** Create an organisation in the database
+    /** Create an organization in the database
      * @param {string} short The short id-like name, i.e. 'bl' for 'Baselland'
-     * @param {string} name The long-form name of the organisation, i.e. 'Bildungswesen Baselland'
-     * @returns {Promise<Organisation>}, a class instance of the created organisation in the database
+     * @param {string} name The long-form name of the organization, i.e. 'Bildungswesen Baselland'
+     * @returns {Promise<Organization>}, a class instance of the created organization in the database
      * */
-    static async createOrganisation(short: string, name: string) {
+    static async createOrganization(short: string, name: string) {
         const uuid = generateUUID();
         await insert(
-            "organisations",
-            ["organisation_id", "short", "name"],
+            "organizations",
+            ["organization_id", "short", "name"],
             uuid,
             [short, name],
         );
-        return new Organisation(uuid);
+        return new Organization(uuid);
     }
 
-    /** Get the short id of the organisation
-     * @returns {string}, the short id of the organisation
+    /** Get the short id of the organization
+     * @returns {string}, the short id of the organization
      * */
     async getShort(): Promise<string> {
         return await this.get("short");
     }
-    /** Change the short id of the organisation
+    /** Change the short id of the organization
      * @param {string} short the to be set short id
      * */
     async setShort(short: string) {
         await this.set("short", short);
     }
-    /** Get the full name of the organisation
-     * @returns {string | null}, the full name of the organisation
+    /** Get the full name of the organization
+     * @returns {string | null}, the full name of the organization
      * */
     async getName() {
         return await this.get("name");
     }
-    /** Change the full name of the organisation
+    /** Change the full name of the organization
      * @param {string} name the to be set full name
      * */
     async setName(name: string) {
